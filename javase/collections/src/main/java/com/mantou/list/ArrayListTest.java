98e5e0b1f7c831a3a2828c725af1d9ea822939b4
mantou <mt15027896686@163.com>
2021-11-04 18:35:03 +0800

    修改注释

--- javase/collections/src/main/java/com/mantou/list/ArrayListTest.java
+++ javase/collections/src/main/java/com/mantou/list/ArrayListTest.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
  * @desc ArrayList集合源码
  * JDK1.7是创建时初始化长度为10，之后每次扩容为之前的1.5倍
  * JDK1.8是调用add方法的时候才会初始化长度为10
- *
  */
 public class ArrayListTest {
     public static void main(String[] args) {
